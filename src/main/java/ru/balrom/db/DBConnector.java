2f6615cb3f2b956217eb04033d63cb17173556d0
balrom1981 <balrom@bk.ru>
2025-02-21 17:41:25 +0300

    DBConnector added

---
+++ src/main/java/ru/balrom/db/DBConnector.java
@@ -0,0 +1,17 @@
+package ru.balrom.db;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class DBConnector {
+    private static final String DRIVER = "com.mysql.cj.jdbc.Driver";
+    private static final String URL = "jdbc:mysql://localhost:3306/aston";
+    private static final String USER = "lastuser";
+    private static final String PASSWORD = "lastuser";
+
+    public Connection getConnection() throws ClassNotFoundException, SQLException {
+        Class.forName(DRIVER);
+        return DriverManager.getConnection(URL, USER, PASSWORD);
+    }
+}
